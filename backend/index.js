0c8a51df21a0bf268d07831b8b3766b98161c712
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2023-12-30 17:27:57 +0530

    course udemy starting up

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,15 @@
-const express = require("express");
-const cors = require('cors')
+const http = require("http");
 require("dotenv").config();
-const app = express(); // creating an instance of express
-app.use(cors());
+// const cors = require('cors');;
 
-app.get("/truths", (req, res) => {
-  const data = {
-    isError: false,
-    statusCode: 200,
-    errorMessage: "",
-    data: [
-      {
-        author: "Nicolas Taleb",
-        truth:
-          "“A Stoic is someone who transforms fear into prudence, pain into transformation, mistakes into initiation, and desire into undertaking.”",
-        year: 2015,
-      },
-      {
-        author: "Seneca",
-        truth:
-          "“Not how long, but how well you have lived is the main thing.”",
-        year: 1100,
-      },
-    ],
-  };
-  res.send(JSON.stringify(data));
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" }); // for setting up the response headers
+  res.write(
+    JSON.stringify({
+      name: "Nihil",
+    })
+  );
+  res.end();
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
-});
+server.listen(process.env.PORT);
