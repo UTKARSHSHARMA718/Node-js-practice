ff31ceebf420b714d7753a0c43e9a7921f60c3e0
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2023-12-31 16:04:49 +0530

    write data in a file using Buffer

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,46 @@
-const express = require("express");
+const http = require("http");
+const fs = require('fs');
 require("dotenv").config();
-const cors = require("cors");
-const app = express();
-const { connectWithDatabase } = require("./db/connect");
 
-app.use(cors());
+const PORT = process.env.PORT || 3030;
+http
+  .createServer((req, res) => {
+    const { method, url } = req;
+    console.log({ method, url });
+    if (url === "/") {
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<head>");
+      res.write("<title> Learning </title>");
+      res.write(`<body> 
+        <form action
+        ="/send_name" method="POST">
+        <input type="text" name="name"/>
+        <button type="submit">
+        submit
+        </button>
+        </form>
+      </body>`);
+      res.write("</title>");
+      res.write("</head>");
+      res.write("</html>");
+      return res.end();
+    }
 
-const all_products_routes = require("./routes/products");
-app.use("/api/products", all_products_routes);
-
-const PORT = process.env.PORT || 9090;
-
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world!");
-});
-
-const startServer = async () => {
-  try {
-    await connectWithDatabase();
-    app.listen(PORT, () => {
-      console.log("listening at 3001");
-    }); 
-  } catch (err) {
-    console.log("err", err);
-  }
-};
-
-startServer();
+    if(url === "/send_name") {
+      //using buffer
+      const data = [];
+      req.on('data',(data_in_form_of_chunk)=>{
+        data.push(data_in_form_of_chunk);
+        console.log({data_in_form_of_chunk})
+      })
+      req.on('end',()=>{
+        const bodyData = Buffer.concat(data).toString();
+        const actualMessage = bodyData.split("=")[1];
+        console.log({actualMessage});
+        fs.writeFileSync("data.txt", actualMessage); // why Syn?
+      })
+      return res.end();
+    }
+  })
+  .listen(PORT);
