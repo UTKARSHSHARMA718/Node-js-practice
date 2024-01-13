f5b390fc28a519397f2c61829ce478b0c9733ac0
UTKARSH SHARMA <86897568+UTKARSHSHARMA718@users.noreply.github.com>
2024-01-14 00:28:53 +0530

    added code for very basic express introduction

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,27 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3030;
-http
-  .createServer((req, res) => {
-    const { method, url } = req;
-    console.log({ method, url });
-    if (url === "/") {
-      res.setHeader("Content-Type", "text/html");
-      res.write("<html>");
-      res.write("<head>");
-      res.write("<title> Learning </title>");
-      res.write(`<body> 
-        <form action
-        ="/send_name" method="POST">
-        <input type="text" name="name"/>
-        <button type="submit">
-        submit
-        </button>
-        </form>
-      </body>`);
-      res.write("</title>");
-      res.write("</head>");
-      res.write("</html>");
-      return res.end();
-    }
+const app = express();
 
-    if (url === "/send_name") {
-      //using buffer
-      const data = [];
-      req.on("data", (data_in_form_of_chunk) => {
-        data.push(data_in_form_of_chunk);
-        console.log({ data_in_form_of_chunk });
-      });
-      req.on("end", () => {
-        const bodyData = Buffer.concat(data).toString();
-        const actualMessage = bodyData.split("=")[1];
-        console.log({ actualMessage });
-        fs.writeFileSync("data.txt", actualMessage); // why Syn?
-      });
-      res.statusCode = 302;
-      res.setHeader("Location", "/superMan");
-      return res.end();
-    }
+app.use("/users", (req, res, next) => {
+  console.log("/users");
+  res.status(400).json({
+    data: [
+      {
+        name: "utlarsh",
+        rollNo: 123456789,
+        class: "10th",
+        section: "A",
+      },
+    ],
+  });
+});
 
-    if(url === "/superMan"){
-      console.log("got it")
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.write("Got it!"); 
-      return res.end();
-    }
-  })
-  .listen(PORT);
+app.use("/", (req, res, next) => {
+  console.log("Home");
+  res.status(200).send("<h1> hello Home </h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("listening on port" + process.env.PORT);
+});
